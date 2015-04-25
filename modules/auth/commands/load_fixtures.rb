d7ea883fdba693a1af21bd7829ea31b9f437c998
Onur Eren Elibol <onur.elibol@ulkem.com.tr>
2015-04-26 00:28:14 +0300

    Fixture loader extended

--- modules/auth/commands/load_fixtures.rb
+++ modules/auth/commands/load_fixtures.rb
@@ -1,19 +1,21 @@
 class Auth::Commands::LoadFixtures < Neo::Command
-	Cypher = Neo::Database::Cypher
+  Cypher = Neo::Database::Cypher
 
   def run
-	  Cypher.new.query('OPTIONAL MATCH (n:$)-[r]-(m:$) DELETE n,r,m').run
-	  Cypher.new.query('MATCH (n:$) DELETE n').run
-
-    fixture_paths = [
-      'roles.yml'
-    ].map{|path| File.join Neo.app_dir, 'modules/auth/fixtures', path}
+    Cypher.new.query('OPTIONAL MATCH (n:$)-[r]-(m:$) DELETE n,r,m').run
+    Cypher.new.query('MATCH (n:$) DELETE n').run
 
     fixtures = Neo::Fixture.new(
-	    paths: fixture_paths,
-	    sort_cols: {'Auth/Role'=> ['name']}
+      paths: fixture_files(['roles.yml'], in_path: 'modules/auth/fixtures'),
+      sort_cols: {'Auth/Role'=> ['name']}
     )
 
     fixtures.load
+  end
+
+  private
+
+  def fixture_files(files, in_path:)
+    files.map{|path| File.join Neo.app_dir, in_path, path}
   end
 end
